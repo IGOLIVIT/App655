e39fb30ab14a8c9fb8b686cfe3c049db42485683
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-07-04 13:23:00 +0300

    upd

---
+++ App655/AllFiles/Extensions/d.swift
@@ -0,0 +1,15 @@
+//
+//  d.swift
+//  App655
+//
+//  Created by IGOR on 04/07/2024.
+//
+
+struct DataManager {
+    
+    let appHudID: String = "app_SyAvoGSbCwWLqo6gnhorarNjTKV3KY"
+    let metricaID: String = "59d7d68c-5c4a-4caa-8f19-1082ef381138"
+    
+    let storage_domain: String = "https://appstorage.org/api/conf/m1ndm41d"
+    let usagePolicy: String = "https://www.termsfeed.com/live/66a4997e-9141-4c3b-90fc-33dfd90d19a5"
+}
